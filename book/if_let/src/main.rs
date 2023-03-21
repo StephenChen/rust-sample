e5c5846bb11892f41392c6d02346f4721b8e2c20
StephenChen <stephenchen.me@gmail.com>
2023-03-21 09:40:19 +0800

    UPDATE: update project

--- book/if_let/src/main.rs
+++ book/if_let/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let mut count = 0;
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    } else {
+        count += 1;
+    }
 }
