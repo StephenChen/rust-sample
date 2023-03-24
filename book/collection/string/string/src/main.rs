f4c53b2119710f1933128ecff88228a4cc691f6e
StephenChen <stephenchen.me@gmail.com>
2023-03-24 17:50:29 +0800

    UPDATE: update project

---
+++ book/collection/string/string/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s2}");
+}
