ea71632c67d4ab4c64a5399de362b3ed2b20f5f8
StephenChen <stephenchen.me@gmail.com>
2023-03-29 18:18:29 +0800

    UPDATE: update project

--- book/generics_trait_lifetimes/generics/src/main.rs
+++ book/generics_trait_lifetimes/generics/src/main.rs
@@ -14,6 +14,20 @@ fn main() {
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let integer = PointOne { x: 5, y: 10 };
+    let float = PointOne { x: 1.0, y: 4.0 };
+    let both_integer = PointTwo { x: 5, y: 10 };
+    let both_float = PointTwo { x: 1.0, y: 4.0 };
+    let integer_and_float = PointTwo { x: 5, y: 4.0 };
+
+    let p = PointOne { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+
+    let p1 = PointTwo { x: 5, y: 10.4 };
+    let p2 = PointTwo { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
 
 fn largest<T: PartialOrd>(list: &[T]) -> &T {
@@ -50,6 +64,37 @@ fn largest_char(list: &[char]) -> &char {
     }
 
     largest
+}
+
+struct PointOne<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> PointOne<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl PointOne<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct PointTwo<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<X1, Y1> PointTwo<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: PointTwo<X2, Y2>) -> PointTwo<X1, Y2> {
+        PointTwo {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 
