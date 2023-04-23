fe69025151bdaf773ef03c0360c273ff9b5f662e
StephenChen <stephenchen.me@gmail.com>
2023-04-23 18:12:42 +0800

    UPDATE: update project

--- book/generics_trait_lifetimes/lifetimes/src/main.rs
+++ book/generics_trait_lifetimes/lifetimes/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 fn main() {
     println!("Hello, world!");
 
@@ -15,9 +17,35 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("call. ago...");
+    let first_sentence = novel.split('.').next().expect("could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence
+    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+    where
+        T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
