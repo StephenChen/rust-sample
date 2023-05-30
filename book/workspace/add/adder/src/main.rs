28c13c9afa45371884933238edaa0e608c1df474
StephenChen <stephenchen.me@gmail.com>
2023-05-30 17:04:33 +0800

    UPDATE: update project

---
+++ book/workspace/add/adder/src/main.rs
@@ -0,0 +1,10 @@
+use add_one;
+use add_two;
+
+fn main() {
+    println!("Hello, world!");
+
+    let num = 10;
+    println!("{num} plus one is {}", add_one::add_one(num));
+    println!("{num} plus two is {}", add_two::add_two(num));
+}
