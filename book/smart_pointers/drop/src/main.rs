c59f93992dd04e2bc062d0c5a131faad927e7e45
StephenChen <stephenchen.me@gmail.com>
2023-06-06 20:50:33 +0800

    UPDATE: update project

---
+++ book/smart_pointers/drop/src/main.rs
@@ -0,0 +1,23 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`", self.data);
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    std::mem::drop(c);
+    println!("CustomSmartPointer c dropped before the end of main.")
+}
