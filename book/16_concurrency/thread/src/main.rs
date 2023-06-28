c10dfaf8cc5c4f6e652f904aa07aee67610e117a
StephenChen <stephenchen.me@gmail.com>
2023-06-28 17:52:46 +0800

    UPDATE: update project

--- book/16_concurrency/thread/src/main.rs
+++ book/16_concurrency/thread/src/main.rs
@@ -1,3 +1,23 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     println!("Hello, world!");
+    spawn();
+}
+
+fn spawn() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("number {} from the spawned thread.", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("number {} from the main thread.", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
 }
