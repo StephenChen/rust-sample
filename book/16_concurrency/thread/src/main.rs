225aa59efeddd46ef093a11319ffb9aa98d85ada
StephenChen <stephenchen.me@gmail.com>
2023-06-29 20:50:24 +0800

    UPDATE: update project

--- book/16_concurrency/thread/src/main.rs
+++ book/16_concurrency/thread/src/main.rs
@@ -20,4 +20,12 @@ fn spawn() {
     }
 
     handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle1 = thread::spawn(move || {
+        println!("vector: {:?}", v);
+    });
+
+    handle1.join().unwrap();
 }
